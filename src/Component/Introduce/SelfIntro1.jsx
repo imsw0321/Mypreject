63cb724c6f640941081dc601f0d464378d5cd594
김선우 <imsw0321@gachon.ac.kr>
2023-11-15 06:48:19 +0900

    11.15.commit.333

--- src/Component/Introduce/SelfIntro1.jsx
+++ src/Component/Introduce/SelfIntro1.jsx
@@ -7,7 +7,7 @@ function SelfIntro1() {
 
             <div className="Intro_box_self">
                 <p>
-                <img src={process.env.PUBLIC_URL + "/mation.png"} width="50px" height="50px" alt="sw" />
+                <img src={process.env.PUBLIC_URL + "/mation.png"} width="20px" height="18px" alt="infor" />
                 기본정보</p>
                 <hr></hr>
                 <p>김선우 Kim Sun Woo</p>
@@ -17,7 +17,9 @@ function SelfIntro1() {
                 <p>본전공: 경영학과 | 복수전공: 컴퓨터공학과</p>
             </div>
             <div className="Intro_box_self">
-                <p>학력사항</p>
+                <p>
+                <img src={process.env.PUBLIC_URL + "/school.png"} width="20px" height="18px" alt="sch" />
+                학력사항</p>
                 <hr></hr>
                 <p>2010.03~2016.02 대전구봉초등학교 졸업</p>
                 <p>2016.03~2019.02 대전봉우중학교 졸업</p>
--- src/Component/Introduce/SelfIntro2.jsx
+++ src/Component/Introduce/SelfIntro2.jsx
@@ -6,7 +6,9 @@ function SelfIntro2() {
         <div className="Maincontent_SelfIntro">
 
             <div className="Intro_box_self">
-                <p>기술</p>
+                <p>
+                <img src={process.env.PUBLIC_URL + "/technical.png"} width="20px" height="20px" alt="tech" />
+                    기술</p>
                 <hr></hr>
                 <p>C언어 ★★☆☆</p>
                 <p>React ★★★☆☆</p>
@@ -14,7 +16,9 @@ function SelfIntro2() {
                 <p>Rcode ★★★☆☆</p>
             </div>
             <div className="Intro_box_self">
-                <p>자격증</p>
+                <p>
+                <img src={process.env.PUBLIC_URL + "/card.png"} width="20px" height="18px" alt="card" />
+                    자격증</p>
                 <hr></hr>
                 <p>2022.06 운전면허 2종</p>
                 <p>2023.07 컴퓨터활용능력 1급 필기</p>
